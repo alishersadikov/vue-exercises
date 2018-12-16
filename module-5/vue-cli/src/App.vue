dc967ad0b4af9c6828100cb54b9df4289eed4066
Alisher Sadikov <asadikov@msts.com>
2018-12-16 15:51:52 -0600

    Complete module 5

---
+++ module-5/vue-cli/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-servers>Server Status: {{ status }}</app-servers>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        status: 'Critical'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
