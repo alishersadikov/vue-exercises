68ba4e82d1bb9ac6a3fdd2763bd5044bc06cb9d0
Alisher Sadikov <asadikov@msts.com>
2018-12-29 21:02:39 -0600

    Final project - enable loading data

--- module-18-Final-project/src/main.js
+++ module-18-Final-project/src/main.js
@@ -8,7 +8,7 @@ import store from './store/store';
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
-Vue.http.options.root = 'https://vue-js-stock-trader-e1e14.firebaseio.com/';
+Vue.http.options.root = 'https://stock-trader-95bd1.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return `$${value.toLocaleString()}`;
